5ad212b30e844c04855f9ecb77bdf9c09b3bac63
sourabhxyz <lambdaproofs@pm.me>
2026-02-18 17:40:25 +0530

    feat: instances for fieldelement rollupbfinterpreter

--- rollup-aggregator-server/src/ZkFold/Cardano/Rollup/Aggregator/Orphans.hs
+++ rollup-aggregator-server/src/ZkFold/Cardano/Rollup/Aggregator/Orphans.hs
@@ -5,18 +5,24 @@ module ZkFold.Cardano.Rollup.Aggregator.Orphans () where
 import Control.Lens (at, (%~), (&), (.~), (?~))
 import Data.HashMap.Strict.InsOrd qualified as IOHM
 import Data.OpenApi
-import Data.Text (Text)
+import Data.Text (Text, unpack)
+import GHC.Natural (Natural)
 import Servant
 import Servant.Foreign
 import Servant.OpenApi
+import Text.Read (readMaybe)
+import ZkFold.Algebra.Class (fromConstant)
 import ZkFold.Cardano.Rollup.Aggregator.Auth (APIKeyAuthProtect, apiKeyHeaderText)
 import ZkFold.Symbolic.Data.FieldElement (FieldElement)
 import ZkFold.Symbolic.Ledger.Types.Field (RollupBFInterpreter)
 
 instance ToParamSchema (FieldElement RollupBFInterpreter) where
-  toParamSchema = undefined
+  toParamSchema _ = mempty & type_ ?~ OpenApiInteger
 
-instance FromHttpApiData (FieldElement RollupBFInterpreter)
+instance FromHttpApiData (FieldElement RollupBFInterpreter) where
+  parseQueryParam t = case readMaybe (unpack t) of
+    Nothing → Left $ "Invalid field element (expected non-negative integer): " <> t
+    Just n → Right $ fromConstant (n ∷ Natural)
 
 instance HasOpenApi api ⇒ HasOpenApi (APIKeyAuthProtect :> api) where
   toOpenApi _ =
